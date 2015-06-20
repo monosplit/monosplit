e55468099c582d563de00df1380cc03156314055
Sarp Kaya <sarpkaya@live.com.au>
2015-06-20 23:28:59 +1000

    Yaml configuration is added

---
+++ src/main/java/Config.java
@@ -0,0 +1,14 @@
+/**
+ * Created by sarp on 20/06/15.
+ */
+public class Config {
+    private String projectPath;
+
+    public String getProjectPath() {
+        return projectPath;
+    }
+
+    public void setProjectPath(String projectPath) {
+        this.projectPath = projectPath;
+    }
+}
--- src/main/java/MonoSplit.java
+++ src/main/java/MonoSplit.java
@@ -1,4 +1,8 @@
+import org.yaml.snakeyaml.Yaml;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -9,8 +13,10 @@ import java.util.Set;
 public class MonoSplit {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Running the command on " + args[0]);
-        LanguageParser rails = new ParseRails(args[0]);
+        Yaml yaml = new Yaml();
+        Config projectConfig = yaml.loadAs(new String(Files.readAllBytes(Paths.get(args[0]))), Config.class);
+        System.out.println("Running the command on " + projectConfig.getProjectPath());
+        LanguageParser rails = new ParseRails(projectConfig.getProjectPath());
         ServiceSplitter serviceSplitter = new ServiceSplitter(rails);
         //serviceSplitter.getSortedUri().forEach((uri) -> System.out.println(uri));
         String firstUri = serviceSplitter.getSortedUri().get(0);
@@ -19,14 +25,14 @@ public class MonoSplit {
         Set<String> controllerFiles = serviceSplitter.getControllerFiles(controllers);
        // controllerFiles.forEach((con) -> System.out.println(con));
 
-        ProjectCopier firstService = new ProjectCopier(args[0], "../app1", controllerFiles, false).
+        ProjectCopier firstService = new ProjectCopier(projectConfig.getProjectPath(), "../app1", controllerFiles, false).
                 setIP("0.0.0.0").setPort(3001).setEndPoint(firstUri).applyProjectSettings();
         firstService.runProjectCommand();
 
         Map<String,String> remainingUriControllers = serviceSplitter.getRemainingControllersFromURI(firstUri, rails);
         remainingUriControllers.forEach((uri, controller) -> controllerFiles.remove(controller)); //Should check if uri is not used when multiple uri services are used
 
-        ProjectCopier lastService = new ProjectCopier(args[0], "../app0", controllerFiles, true).setIP("0.0.0.0").setPort(3000).applyProjectSettings();
+        ProjectCopier lastService = new ProjectCopier(projectConfig.getProjectPath(), "../app0", controllerFiles, true).setIP("0.0.0.0").setPort(3000).applyProjectSettings();
         lastService.runProjectCommand();
 
         ProxyConfigurator proxyConfigurator = new ProxyConfigurator().addProjectToProxy(firstService).addProjectToProxy(lastService);
